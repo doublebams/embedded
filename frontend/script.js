382788b1ad2c504878ed41a2bb51bb24c897cceb
Maekin <74455990+mekintown@users.noreply.github.com>
2023-05-22 17:15:30 +0700

    Add condition to check for weather

--- frontend/script.js
+++ frontend/script.js
@@ -4,18 +4,23 @@ const light = document.querySelector('.luxValue')
 const weatherImage = document.querySelector('.weatherImageHolder')
 
 function updateValue() {
-    // humidity.innerHTML =
-    // temperature.innerHTML =
-    // light.innerHTML =
-    if (isGoingToRain()) {
-        weatherImage.innerHTML = `<img class="weatherImage" src="./images/storm.png" alt="storm"></img>`
+    // TODO: Connect this data with database
+
+    humidity.innerHTML = 75
+    temperature.innerHTML = 27
+    light.innerHTML = 300
+    // End of ToDo
+    if (light.innerHTML < 300) {
+        weatherImage.innerHTML = `<img class="weatherImage" src="./images/moon.png" alt="moon"></img>`
     }
 
-    else if (isDark()) {
-        weatherImage.innerHTML = `<img class="weatherImage" src="./images/moon.png" alt="moon"></img>`
+    else if (humidity.innerHTML > 70 && temperature.innerHTML > 25) {
+        weatherImage.innerHTML = `<img class="weatherImage" src="./images/storm.png" alt="storm"></img>`
     }
 
     else {
         weatherImage.innerHTML = `<img class="weatherImage" src="./images/sunny.png" alt="sunny">`
     }
 }
+
+updateValue();
