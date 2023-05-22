6028b7aa5d15d5d0a504d27d2d1d810aa6dfd5d6
Maekin <74455990+mekintown@users.noreply.github.com>
2023-05-22 15:57:23 +0700

    Add Frontend

---
+++ frontend/script.js
@@ -0,0 +1,5 @@
+const humidity = document.querySelector('.humidityValue')
+const temperature = document.querySelector('.tempValue')
+const light = document.querySelector('.luxValue')
+const weatherImage = document.querySelector('.weatherImage')
+
