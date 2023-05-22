9d7fee19d9ebe6c2c08ddd9b3800a278fe735b54
Maekin <74455990+mekintown@users.noreply.github.com>
2023-05-22 16:13:06 +0700

    Add function to update value

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,21 @@
 const humidity = document.querySelector('.humidityValue')
 const temperature = document.querySelector('.tempValue')
 const light = document.querySelector('.luxValue')
-const weatherImage = document.querySelector('.weatherImage')
+const weatherImage = document.querySelector('.weatherImageHolder')
 
+function updateValue() {
+    // humidity.innerHTML =
+    // temperature.innerHTML =
+    // light.innerHTML =
+    if (isGoingToRain()) {
+        weatherImage.innerHTML = `<img class="weatherImage" src="./images/storm.png" alt="storm"></img>`
+    }
+
+    else if (isDark()) {
+        weatherImage.innerHTML = `<img class="weatherImage" src="./images/moon.png" alt="moon"></img>`
+    }
+
+    else {
+        weatherImage.innerHTML = `<img class="weatherImage" src="./images/sunny.png" alt="sunny">`
+    }
+}
